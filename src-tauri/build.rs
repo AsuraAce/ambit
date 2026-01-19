2d6100fb1165f9f27192e6229652696b4758759f
Astra orion <13394741+AsuraAce@users.noreply.github.com>
2026-01-19 16:19:47 +0100

    fix(tauri): resolve STATUS_ENTRYPOINT_NOT_FOUND by tracking SKIP_TAURI_BUILD

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,6 +2,7 @@ fn main() {
     if std::env::var("SKIP_TAURI_BUILD").is_err() {
         tauri_build::build()
     } else {
+        println!("cargo:rerun-if-env-changed=SKIP_TAURI_BUILD");
         println!("cargo:warning=Skipping tauri_build::build() because SKIP_TAURI_BUILD is set");
     }
 }
